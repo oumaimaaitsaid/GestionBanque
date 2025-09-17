8f67d325222aec813c2643cfba0690fed5a4ae12
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-09-17 17:24:00 +0100

    KAN-5  ajouter la classe de CompteCourant

--- src/com/banque/model/CompteCourant.java
+++ src/com/banque/model/CompteCourant.java
@@ -1,5 +1,23 @@
 package com.banque.model;
 
-public class CompteCourant {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class CompteCourant extends Compte{
+	
+	private double decouvert;
+	
+	CompteCourant (String code,double solde,double decouvert){
+		super(code,solde);
+		this.decouvert=decouvert;
+	}
+	
+	
+	
+
+	
+	
+	
+	
 
 }
