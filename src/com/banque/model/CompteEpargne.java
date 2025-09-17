3672d224fd4790c31ecc2d562044b9ce3ca309f9
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-09-17 22:17:40 +0100

    KAN-6 ajouter class CompteEpargne

--- src/com/banque/model/CompteEpargne.java
+++ src/com/banque/model/CompteEpargne.java
@@ -1,5 +1,22 @@
 package com.banque.model;
 
-public class CompteEpargne {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class CompteEpargne extends Compte {
+	
+	private double tauxInteret;
+	
+	CompteEpargne (String code,double solde,double tauxInteret){
+		super(code,solde);
+		this.tauxInteret=tauxInteret;
+	}
+
+	
+	
+	
+	
+	
+	
 
 }
