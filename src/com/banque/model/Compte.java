4915f06cb334d5d86ac40cedaf0d7a16670eb10f
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-09-17 16:27:17 +0100

    KAN-7 ajouter method generateCode

--- src/com/banque/model/Compte.java
+++ src/com/banque/model/Compte.java
@@ -1,5 +1,27 @@
 package com.banque.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
 public abstract class Compte {
 
+    private String code;
+    private double solde;
+    private List<Operation> listOperations =new ArrayList<>();
+    public Compte(String code, double soldeInitial) {
+        
+        this.code = generateCode();
+        this.solde = soldeInitial;
+    }
+    
+    //generateCode.Automaticly
+    public String generateCode() {
+    	long value =Math.abs(UUID.randomUUID().getMostSignificantBits());
+    	long fiveDigits =value % 100000;
+    	return String.format("CPT-%05d",fiveDigits);
+    }
+    //methods abstract
 }
