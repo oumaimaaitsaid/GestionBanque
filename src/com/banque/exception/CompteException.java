fc1484b0e00247ad0383a856afa5715d5814b6be
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-09-19 11:42:41 +0100

    ajouter le fichier jar

--- src/com/banque/exception/CompteException.java
+++ src/com/banque/exception/CompteException.java
@@ -2,6 +2,7 @@ package com.banque.exception;
 
 
 public class CompteException extends Exception {
+	private static final long serialVersionUID = 1L;
     public CompteException(String message) {
         super(message);
     }
